8b22d5a64ea7fbe0c7b25697be8bd72666596fe8
908pierpeach <pierpeach@gmail.com>
2026-02-27 21:10:08 -0500

    Made correlation file. Starting on lead_lag.py

--- src/lead_lag.py
+++ src/lead_lag.py
@@ -5,5 +5,3 @@ project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 df = pd.read_csv(os.path.join(project_root, "data", "processed", "returns_combined.csv"), index_col="Date")
 processed_dir = os.path.join(project_root, "data", "processed")
 
-corr_matrix = df.corr()
-corr_matrix.to_csv(os.path.join(processed_dir, "correlation_matrix.csv"))
