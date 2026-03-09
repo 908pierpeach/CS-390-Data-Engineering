f0fde70fef630d64208b5c0c8e32d6235bbc0fb0
908pierpeach <pierpeach@gmail.com>
2026-03-08 22:16:42 -0400

    set up drawdown computations

--- src/drawdown.py
+++ src/drawdown.py
@@ -7,13 +7,19 @@ df = pd.read_csv(os.path.join(project_root, "data", "processed", "returns_combin
 processed_dir = os.path.join(project_root, "data", "processed")
 
 
-results = []
+max_results = []
+series_results = {}
+
 for ticker in df.columns:
         cumulative = np.exp(df[ticker].cumsum())
         running_peak = cumulative.cummax()
         drawdown = (cumulative / running_peak) - 1
         max_drawdown = drawdown.min()
-        results.append({"ticker": ticker, "max_drawdown": max_drawdown})
+        max_drawdown_date = drawdown.idxmin()
+        series_results[ticker] = drawdown
+        max_results.append({"ticker": ticker, "max_drawdown": max_drawdown, "date": max_drawdown_date})
 
-drawdown_df = pd.DataFrame(results)
-drawdown_df.to_csv(os.path.join(processed_dir, "max_drawdown.csv"), index=False)
+max_drawdown_df = pd.DataFrame(max_results)
+drawdown_series_df = pd.DataFrame(series_results, index = df.index)
+max_drawdown_df.to_csv(os.path.join(processed_dir, "max_drawdown.csv"), index=False)
+drawdown_series_df.to_csv(os.path.join(processed_dir, "drawdown_series.csv"))
